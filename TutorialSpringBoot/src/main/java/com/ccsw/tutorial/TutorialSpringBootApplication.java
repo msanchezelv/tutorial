65e44d88002c6727823c0a712ca9a1b24cc4cb35
marina31sanchez <U01ABF67@lacaixa.es>
2025-04-04 11:19:57 +0200

    SpringBoot

---
+++ TutorialSpringBoot/src/main/java/com/ccsw/tutorial/TutorialSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ccsw.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSpringBootApplication.class, args);
+    }
+
+}
